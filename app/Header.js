e4278f07fbfdc48d2ea373cef945e41c4c1a945a
prathamesh <prathamesh69bhavsar@gmail.com>
2024-11-25 16:13:42 +0530

    Header added

---
+++ app/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <div>
+                <div>
+                    <h1>LOGO</h1>
+                </div>
+                <div>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Menu</a></li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header
