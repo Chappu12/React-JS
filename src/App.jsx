158914aaac3eb70f0e2873e03a810dae7ad97d5a
prathamesh <prathamesh69bhavsar@gmail.com>
2024-11-27 23:32:25 +0530

    make changes in header and added

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+'use client'
+import React, { useState } from 'react'
+import './App.css'
+
+const App = () => {
+    const [name, setName] = useState("Prathamesh")
+    const [flag, setflag] = useState(0)
+    return (
+        <>
+            <div className='main'>
+                <div>Hi my name is {name}</div>
+                <button className='btn' onClick={() => {
+                    setName((name) => name === "Prathamesh" ? "Developer" : "Prathamesh")
+
+                    // if(flag == 0){
+                    //     setName("Developer")
+                    //     setflag(1)
+                    // }
+                    // else{
+                    //     setName("Prathamesh")
+                    //     setflag(0)
+                    // }
+                }}>Click me</button>
+            </div>
+        </>
+    )
+}
+
+export default App
+
+
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,24 +1,24 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
-  return (
-    <>
-        <div className='nav-bar'>
-            <div>
-                LOGO
-            </div>
-            <div>
-                <ul className='nav-menu'>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Product</li>
-                    <li>Sign up</li>
-                </ul>
-            </div>
-            
-        </div>
-    </>
-  )
+    return (
+        <>
+            <nav className='nav-bar'>
+                <div>
+                    LOGO
+                </div>
+                <div>
+                    <ul className='nav-menu'>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Product</li>
+                        <li>Sign up</li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    )
 }
 
 export default Header
