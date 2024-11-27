111e64deded086dbc356c273870a5bd59260f01b
prathamesh <prathamesh69bhavsar@gmail.com>
2024-11-27 22:04:40 +0530

    commit done

---
+++ Components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+        <nav>
+            <div>
+                LOGO
+            </div>
+            <div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Product</li>
+                    <li>Sign up</li>
+                </ul>
+            </div>
+            
+        </nav>
+    </>
+  )
+}
+
+export default Header
