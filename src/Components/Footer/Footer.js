0c78cad59da2428c3f444eb041d95f249d52f442
prathamesh <prathamesh69bhavsar@gmail.com>
2024-12-12 11:29:49 +0530

    make changes in footer

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <>
+        <div className='footer'>@CopyWrite Footer</div>
+    </>
+  )
+}
+
+export default Footer
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
     <App />
+    <Footer />
   </React.StrictMode>
 );
 
