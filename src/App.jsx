98836da936c108ae9500c36fad55a0c4a10b8ae0
prathamesh <prathamesh69bhavsar@gmail.com>
2024-12-12 18:25:01 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import { Link } from "react-router-dom";
 const App = () => {
     const [name, setName] = useState("Prathamesh")
     const [counter, setCounter] = useState(0)
+    const ele = <h1>MAD MAX</h1>
     // const [flag, setflag] = useState(0)
     return (
         <>
             <div className='main'>
+                {ele}
                 <div>Hi my name is {name}</div>
                 <button className='btn' onClick={() => {
                     setName((name) => name === "Prathamesh" ? "Developer" : "Prathamesh")
@@ -22,7 +24,7 @@ const App = () => {
                     //     setflag(0)
                     // }
                 }}>Click me</button>
-                <div>
+                <div className='count'>
                     <p>Counter : {counter}</p>
                     <button className="btn" onClick={() => {
                         setCounter(counter + 1)
@@ -30,10 +32,10 @@ const App = () => {
                         Increment
                     </button>
                     <button className="btn" onClick={() => {
-                        if(counter <= 0){
+                        if (counter <= 0) {
                             alert("Less than 0 not allowed")
                         }
-                        else{
+                        else {
                             setCounter(counter - 1)
                         }
                     }}>Decrement</button>
